58b72797ff198ec67cdec500878b26bb79d8e1c0
Nafanua <serjoid2601@hotmail.com>
2018-12-22 22:22:11 +0200

    Added icon

--- Forms/GameField.Designer.cs
+++ Forms/GameField.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameField));
             this.button0 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -146,6 +147,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button0);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GameField";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
--- Forms/SrartForm.Designer.cs
+++ Forms/SrartForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
             this.PvP = new System.Windows.Forms.Button();
             this.PvA = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -95,6 +96,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.PvA);
             this.Controls.Add(this.PvP);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StartForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "XO";
