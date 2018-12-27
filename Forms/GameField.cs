dff1576c917489eca645551940b28d1da37ef0e7
Nafanua <serjoid2601@hotmail.com>
2018-12-27 02:08:31 +0200

    code refactoring

--- Forms/GameField.cs
+++ Forms/GameField.cs
@@ -22,8 +22,8 @@ namespace XO.Forms
         private int _oWins;
         private int _deadHeats;
 
-        private static readonly int[][] WinRules = new int[8][]
-            {new [] {0, 1, 2}, new [] {3, 4, 5}, new [] {6, 7, 8}, new [] {0, 3, 6}, new [] {1, 4, 7}, new [] {2, 5, 8}, new [] {0, 4, 8}, new [] {2, 4, 6}};
+        private static readonly int[][] WinRules = {new [] {0, 1, 2}, new [] {3, 4, 5}, new [] {6, 7, 8}, new [] {0, 3, 6},
+                                                    new [] {1, 4, 7}, new [] {2, 5, 8}, new [] {0, 4, 8}, new [] {2, 4, 6}};
 
         private void NextMove() => _turn = _turn == X ? O : X;
 
